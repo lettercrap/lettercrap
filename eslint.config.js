fbda3381784608293d38c15dc675d7c348e68f5d
dabico <dabico@usi.ch>
2024-08-06 14:26:35 +0200

    Migrate ESLint config to new flat variant from ESLint 9

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+import js from '@eslint/js';
+import globals from 'globals';
+import { FlatCompat } from '@eslint/eslintrc';
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'package*.json', 'example/lettercrap.min.*'],
+  },
+  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'),
+  {
+    plugins: { '@typescript-eslint': typescriptEslint },
+    languageOptions: {
+      parser: tsParser,
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+];
