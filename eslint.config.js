1686f7d894625bcac9dd48d6a6ef482266ad6118
dabico <dabico@usi.ch>
2024-08-06 15:40:23 +0200

    Finalize ESLint config migration

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,19 @@
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-import js from '@eslint/js';
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 import globals from 'globals';
-import { FlatCompat } from '@eslint/eslintrc';
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+const eslintRecommended = eslint.configs.recommended;
+const eslintPluginTypeScriptRecommended = tseslint.configs.recommended;
 
 export default [
   {
     ignores: ['dist/**', 'node_modules/**', 'package*.json', 'example/lettercrap.min.*'],
   },
-  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended'),
+  eslintRecommended,
+  ...eslintPluginTypeScriptRecommended,
   eslintPluginPrettierRecommended,
   {
     plugins: { '@typescript-eslint': typescriptEslint },
